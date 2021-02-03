0bb5acfab07caab7e6fa682dcaa251b6e607b052
lovvp <67538804+lovvp@users.noreply.github.com>
2021-02-03 23:03:10 +0900

    hottopic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import HotTopic from "./components/HotTopic";
 import "./App.css";
-
+import { fakedata } from "./datas/fakedata";
 function App() {
+	const [items] = useState(fakedata.items);
 	return (
 		<div>
-			<newFights />
+			<div className="hot-all">
+				<HotTopic FakeData={items} />
+			</div>
 		</div>
-)
+	);
 }
+
+export default App;
