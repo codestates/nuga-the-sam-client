2961e29bb400753f5a44f4c74c459789b4f08f7d
lovvp <67538804+lovvp@users.noreply.github.com>
2021-02-08 18:00:47 +0900

    login and signup modal

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Loading() {
 	console.log(true);
-	return <div>로딩중입니다</div>;
+	return (
+		<div>
+			<div className="loadingModal">로딩중입니다</div>
+		</div>
+	);
 }
 
 export default Loading;
